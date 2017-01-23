af57e24485a51d63fdb54ff0b7e7f4c15f47f7f8
Midhun Dandamudi <midhunsezhi@gmail.com>
2017-01-22 17:22:49 -0800

    Create and style the header part of the portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,16 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     minifyCSS = require('gulp-minify-css');
-
-gulp.task('scripts', function() {
-    gulp.src('js/*.js')
-        .pipe(concat())
-        .pipe(uglify())
-        .pipe(rename('app.min.js'))
-        .pipe(gulp.dest('js/'));
-});
 
 gulp.task('styles', function() {
     gulp.src('css/*.css')
         .pipe(minifyCSS())
         .pipe(rename('style.min.css'))
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('dest/'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['scripts']);
     gulp.watch('css/*.css', ['styles']);
 });
 
-gulp.task('default', ['scripts', 'styles']);
+gulp.task('default', ['styles']);
